a8734e38771fa65a0c9650b61fa2b90f6def8ced
rafaelputaro <rafaelputaro@gmail.com>
2025-10-24 19:19:31 -0300

    working on prometheus

--- README.md
+++ README.md
@@ -90,3 +90,5 @@ https://last9.io/blog/docker-monitoring-with-prometheus-a-step-by-step-guide/
 https://youtu.be/WUBjlJzI2a0
 
 La IA me sugirió: Usar una herramienta de visualización de grafos como Grafana con panel de grafos para ver el derrotero de los mensajes en los nodos.
+
+http://localhost:9090/
--- src/client/main.go
+++ src/client/main.go
@@ -9,11 +9,36 @@ import (
 	filetransfer "tp/common/files_common/file_transfer"
 	"tp/common/keys"
 	rpc_ops_common "tp/common/rpc_ops"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 const MESSAGE_START = "Starting client..."
 
+type metrics struct {
+	addFile prometheus.Gauge
+}
+
+func NewMetrics(reg prometheus.Registerer) *metrics {
+	m := &metrics{
+		addFile: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: "client",
+			Name:      "add_file",
+			Help:      "Number of add file requests",
+		}),
+	}
+	reg.MustRegister(m.addFile)
+	return m
+}
+
 func main() {
+	/*	reg := prometheus.NewRegistry()
+		m := NewMetrics(reg)
+		m.addFile.Set(float64(1))
+		promHandler := promhttp.HandlerFor(reg, promhttp.HandlerOpts{})
+		http.Handle("/metrics", promHandler)
+	*/
+
 	// iniciar cliente
 	common.Log.Info(MESSAGE_START)
 	common.InitLogger()
